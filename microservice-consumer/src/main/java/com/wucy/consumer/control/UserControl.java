8584a3a277c6c8ef9a377bb341ed62445b5c9ba8
wucy <wucy1991@163.com>
2017-06-19 17:12:57 +0800

    get service instance

--- microservice-consumer/src/main/java/com/wucy/consumer/control/UserControl.java
+++ microservice-consumer/src/main/java/com/wucy/consumer/control/UserControl.java
@@ -3,10 +3,14 @@ package com.wucy.consumer.control;
 import com.wucy.consumer.entity.User;
 import com.wucy.consumer.service.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by wucy on 2017/6/16.
@@ -18,6 +22,8 @@ public class UserControl {
     @Autowired
     private UserFeignClient userFeignClient;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping(value = "/{id}")
     public User getUserById(@PathVariable Integer id){
@@ -28,6 +34,17 @@ public class UserControl {
     @GetMapping(value = "/hello")
     public String hello(){
         return "hello";
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping(value = "/getServiceInstance")
+    public List<ServiceInstance> getServiceInstance(){
+        return discoveryClient.getInstances("MICROSERVICE-PROVIDER");
+
     }
 
 }
